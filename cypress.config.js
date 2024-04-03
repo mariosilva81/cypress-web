4b4856b68652cebd41658680a15f23b871c45588
mariosilva81 <mariosilva81@icloud.com>
2024-04-03 09:44:10 -0300

    feat: create cypress cloud integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "9nsg8a",
   e2e: {
     baseUrl: "https://automationpratice.com.br",
     setupNodeEvents(on, config) {
